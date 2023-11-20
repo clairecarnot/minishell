8f5f5b6430213f6f15af2ad5fe4b5daf88a6f33a
Claire CARNOT <ccarnot@made-f0Br7s1.clusters.42paris.fr>
2023-11-20 17:15:34 +0100

    env-norminette

--- include/env.h
+++ include/env.h
@@ -6,7 +6,7 @@
 /*   By: ccarnot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:37:07 by ccarnot           #+#    #+#             */
-/*   Updated: 2023/11/20 17:10:08 by ccarnot          ###   ########.fr       */
+/*   Updated: 2023/11/20 17:15:13 by ccarnot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 
 //----------------------- env.c ------------------------
 char	*replace_shlvl(char *old, char *new);
-int	update_shlvl(t_ms *ms);
-int	init_env(t_ms *ms, char **env);
-int	is_var_in_env(t_list *env, char *var);
-int	init_oldwkdir(t_ms *ms);
-int	init_workdir(t_ms *ms);
+int		update_shlvl(t_ms *ms);
+int		init_env(t_ms *ms, char **env);
+int		is_var_in_env(t_list *env, char *var);
+int		init_oldwkdir(t_ms *ms);
+int		init_workdir(t_ms *ms);
 //
 //----------------------- exp.c ------------------------
-int	min_len(char *s1, char *s2);
+int		min_len(char *s1, char *s2);
 void	swap_lst(void **l1, void **l2);
 t_list	*lst_dup(t_list *lst_tocpy);
-int	init_exp(t_ms *ms);
+int		init_exp(t_ms *ms);
 
 #endif
