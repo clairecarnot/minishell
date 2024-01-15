8b87e37f8c3e09e0dbb1bbc9a54277b602a3f1c9
Maelle POIRIER <mapoirie@bess-f2r5s9.clusters.42paris.fr>
2024-01-15 11:09:00 +0100

    cd 15.01

--- srcs/builtins/cd/cmd_cd.c
+++ srcs/builtins/cd/cmd_cd.c
@@ -1,111 +1,6 @@
 #include "../../../include/builtins.h"
 #include "../libft/libft.h"
 
-char	*replace_oldpwd_env2(t_ms *ms, t_cmd *cmd)
-{
-	char	*content_tmp;
-	t_list	*env_tmp;
-
-	env_tmp = ms->env;
-	while (env_tmp)
-	{
-		if (ft_strncmp(env_tmp->content, "PWD", 3) == 0)
-		{
-			content_tmp = ft_strdup(env_tmp->content + 4);
-			if (!content_tmp)// a verifier
-			{
-				free_cmd(cmd);
-				ms->exit_code = 255;
-				free_minishell(ms, 1);
-			}
-			return (content_tmp);
-		}
-		env_tmp = env_tmp->next;
-	}
-	return (NULL);
-}
-
-void	replace_oldpwd_env(t_ms *ms, t_cmd *cmd)
-{
-	char	*content_tmp;
-	char	*content;
-	t_list	*env_tmp;
-
-	env_tmp = ms->env;
-	content_tmp = replace_oldpwd_env2(ms, cmd);
-	content = ft_strjoin("OLDPWD=", content_tmp);
-	if (!content)
-	{
-		free(content_tmp);
-		free_cmd(cmd);
-		ms->exit_code = 255;
-		free_minishell(ms, 1);
-	}
-	// while (env_tmp)
-	// {
-	// 	if (ft_strncmp(env_tmp->content, "PWD", 3) == 0)
-	// 	{
-	// 		content_tmp = ft_strdup(env_tmp->content + 4);
-	// 		if (!content_tmp)// a verifier
-	// 		{
-	// 			free_cmd(cmd);
-	// 			ms->exit_code = 255;
-	// 			free_minishell(ms, 1);
-	// 		}
-	// 		content = ft_strjoin("OLDPWD=", content_tmp);
-	// 		if (!content)
-	// 		{
-	// 			free(content_tmp);
-	// 			free_cmd(cmd);
-	// 			ms->exit_code = 255;
-	// 			free_minishell(ms, 1);
-	// 		}
-	// 		free(content_tmp);
-	// 	}
-	// 	env_tmp = env_tmp->next;
-	// }
-	// env_tmp = ms->env;
-	while (env_tmp)
-	{
-		if (ft_strncmp(env_tmp->content, "OLDPWD", 6) == 0)
-		{
-			free(env_tmp->content);
-			env_tmp->content = content;
-			return;
-		}
-		env_tmp = env_tmp->next;
-	}
-}
-
-void	replace_pwd_env(t_ms *ms, t_cmd *cmd)
-{
-	char	path[1024];
-	char	*content;
-	t_list	*env_tmp;
-
-	env_tmp = ms->env;
-	if (getcwd(path, sizeof(path)) != NULL)
-	{
-		content = ft_strjoin("PWD=", path);
-		if (!content)// a verifier
-		{
-			free_cmd(cmd);
-			ms->exit_code = 255;
-			free_minishell(ms, 1);
-		}
-		while (env_tmp)
-		{
-			if (ft_strncmp(env_tmp->content, "PWD", 3) == 0)
-			{
-				free(env_tmp->content);
-				env_tmp->content = content;
-				return ;
-			}
-			env_tmp = env_tmp->next;
-		}
-	}
-}
-
 char	*get_dir(t_ms *ms, t_cmd *cmd, char *var_line)
 {
 	int		i;
@@ -177,15 +72,18 @@ int	exec_cd(t_ms *ms, t_cmd *cmd)
 			if (chdir(dir) != 0)
 			{
 				free(dir);
-				ft_putstr_fd("bash: cd: ", 2);
-				ft_putstr_fd(cmd->args[1], 2);
-				ft_putstr_fd(": No such file or directory\n", 2);
+				perror("cd");
+				// ft_putstr_fd("cd: ", 2);
+				// ft_putstr_fd(cmd->args[1], 2);
+				// ft_putstr_fd(": No such file or directory\n", 2);
 			}
 			else
 			{
 				free(dir);
 				replace_oldpwd_env(ms, cmd);
+				replace_oldpwd_exp(ms, cmd);
 				replace_pwd_env(ms, cmd);
+				replace_pwd_exp(ms, cmd);
 			}
 			return (1);
 		}
@@ -200,7 +98,9 @@ int	exec_cd(t_ms *ms, t_cmd *cmd)
 			else
 			{
 				replace_oldpwd_env(ms, cmd);
+				replace_oldpwd_exp(ms, cmd);
 				replace_pwd_env(ms, cmd);
+				replace_pwd_exp(ms, cmd);
 			}
 			return (1);
 		}
