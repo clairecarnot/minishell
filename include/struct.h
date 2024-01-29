06702d4be2f9ed3061cf84a5d7e5f1afb5814190
Claire CARNOT <ccarnot@made-f0Cr12s5.clusters.42paris.fr>
2024-01-29 19:04:30 +0100

    norme suite .h 2901

--- include/struct.h
+++ include/struct.h
@@ -4,7 +4,9 @@
 # include "../libft/libft.h"
 
 /*
- * wil : expand de * = 1, non expand = 0
+ * typedef struct s_wil:
+ *
+ * t_list *w : expand de * = 1, non expand = 0
  */
 
 typedef struct s_wil
@@ -13,7 +15,9 @@ typedef struct s_wil
 }	t_wil;
 
 /*
- * dol : expand du $ = 1, non expand = 0
+ * typedef struct s_dol:
+ *
+ * t_list *dol : expand du $ = 1, non expand = 0
  */
 
 typedef struct s_dol
@@ -70,10 +74,10 @@ typedef enum e_node_type
 
 typedef struct s_redirs
 {
-	t_node_type		type;
-	char			*filename;
+	t_node_type			type;
+	char				*filename;
 	struct s_redirs		*next_redir;
-	t_dol			*dol;
+	t_dol				*dol;
 }		t_redirs;
 
 struct	s_ast
@@ -86,7 +90,6 @@ struct	s_ast
 	t_redirs	*redirs;
 	int			*pipe;
 	t_ast		*parent;
-//	t_list		*dol;
 	t_dol		*dol;
 	t_wil		*wil;
 };
@@ -112,10 +115,9 @@ typedef struct s_token
 	t_type			type;
 	char			*value;
 	struct s_token	*next_token;
-	size_t			tok_nb;
-	t_wil		*wil;
-	t_dol		*dol; // expand du $ = 1, non expand = 0
-	//t_list			*dol;// expand du $ = 1, non expand = 0
+//	size_t			tok_nb;
+	t_wil			*wil;
+	t_dol			*dol;
 }		t_token;
 
 typedef struct s_lexer
@@ -123,34 +125,56 @@ typedef struct s_lexer
 	char	*src;
 	size_t	src_size;
 	char	cur_c;
-	
 	size_t	cur_pos;
 	size_t	next_pos;
 	t_token	*token_lst;
-	size_t	tok_count;//nb de token final
+//	size_t	tok_count;//nb de token final A SUPPR
 }		t_lexer;
 
-typedef	struct s_wildcard
+/*
+ *typedef struct s_wildcard :
+ *
+ * int i : index for the position in the arg tab
+ * 	   if we need to do a copy and to add, we need to
+ * 	   do it starting at i and replace i by i+nb of added cases
+ *
+ * int size : size of the previous arg which needs to be advanced later
+ * 	      in the wil lst
+ *
+ * int index : normal index for incrementing purpose
+ *
+ * int j : index in wildcards_2
+ */
+
+typedef struct s_wildcard
 {
-	int		i;//index qui nous indique ou onn se trouve dans notre tableau d'arg
-	//s'il faut faire copie et ajout, il faudra le faire a partir de i et 
-	//remplacer i par i + nb de cases ajoutees
-	int		size;//taille de l'arg preedent qui doit etre avancee plus tard dans la lst wil
-	int		index;//index normal qui incremente
+	int		i;
+	int		size;
+	int		index;
 	char	**tmp_args;
 	int		*tab_w;
 	char	**added_args;
 	char	**new_args;
-	int		j;//index dans wildcards_2
+	int		j;
 	t_wil	*wil;
 
 }	t_wildcard;
 
+/*
+ * typedef struct s_ms:
+ *
+ * int i : index lexer
+ * int j : lexer (norm)
+ * int nb_q : lexer (norm)
+ * int i_w : index wildcard
+ * int a : test malloc
+ */
+
 typedef struct s_ms
 {
-	int			i;//index lexer
-	int			j;//lexer pour norme
-	int			nb_q;//lexer pour norme
+	int			i;
+	int			j;
+	int			nb_q;
 	int			flag_q;
 
 	t_lexer		*lexer;
@@ -169,10 +193,10 @@ typedef struct s_ms
 	int			in;
 	int			out;
 	t_list		*hdlst;
-	int			i_w;//index wildcard
+	int			i_w;
 	char		*value;
 
-	int	a;//test malloc
+	int			a;
 }				t_ms;
 
 typedef struct s_trash
